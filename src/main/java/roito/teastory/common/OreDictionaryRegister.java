b449df16d87cca37a0564c54211debf134cdbf7d
Roito.Leonine <LuoXiaoWings@outlook.com>
2019-02-02 22:56:21 +0800

    唱片矿词词典与配方添加

--- src/main/java/roito/teastory/common/OreDictionaryRegister.java
+++ src/main/java/roito/teastory/common/OreDictionaryRegister.java
@@ -15,6 +15,14 @@ public class OreDictionaryRegister
 		OreDictionary.registerOre("seedTea", ItemRegister.tea_seeds);
 		OreDictionary.registerOre("record", ItemRegister.caichawuqu_g20);
 		OreDictionary.registerOre("record", ItemRegister.caichawuqu_folk);
+		OreDictionary.registerOre("record", ItemRegister.bubugao);
+		OreDictionary.registerOre("record", ItemRegister.caichaji_erhu);
+		OreDictionary.registerOre("record", ItemRegister.chunjiexuqu);
+		OreDictionary.registerOre("record", ItemRegister.huahaoyueyuan);
+		OreDictionary.registerOre("record", ItemRegister.jinshekuangwu);
+		OreDictionary.registerOre("record", ItemRegister.xiyangyang);
+		OreDictionary.registerOre("record", ItemRegister.yangliuqing);
+		OreDictionary.registerOre("record", ItemRegister.zizhudiao);
 		OreDictionary.registerOre("listAllmilk", Items.MILK_BUCKET);
 		OreDictionary.registerOre("listAllwater", Items.WATER_BUCKET);
 		OreDictionary.registerOre("listAllsugar", Items.SUGAR);
